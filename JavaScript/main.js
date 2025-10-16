63c8eb7cd909eff18d049af662c25ebccdf8a054
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-10-16 22:51:13 +0300

    Update main.js

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,5 +1,4 @@
-function innerHTML(p1,p2) {
-  p2 = p2
+function innerHTML(p1, p2) {
   document.getElementById(p1).innerHTML = p2
 }
 let soderzh = '<p>19.09.2025: Вики была создана</p><p>20.09.2025: созданы RuWiki и EnWiki</p><p>21.09.2025: Приветствуем нового админа Вики! теперь Lobo145 админ RuScratchWikiByShaman2016!</p><p>16.10.2025: Начато внедрение 2.0 страниц и JavaScript</p>'
