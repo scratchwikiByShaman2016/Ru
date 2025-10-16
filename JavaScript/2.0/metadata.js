15b37c8e7e18067ce51a8b4df220a026442748c1
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-10-16 23:11:39 +0300

    Create metadata.js

---
+++ JavaScript/2.0/metadata.js
@@ -0,0 +1,12 @@
+function innerHTML(p1, p2) {
+  document.getElementById(p1).innerHTML = p2
+}
+function TextContent(p1, p2) {
+  document.getElementById(p1).textContent = p2
+}
+const url = new URL(window.location)
+const searchParams = url.searchParams
+if searchParams.get('id') == '1' {
+  TextContent('ogtitle', '2.0 страницы')
+  TextContent('ogsitetitle', 'RuScratchWikiByShaman2016')
+}
