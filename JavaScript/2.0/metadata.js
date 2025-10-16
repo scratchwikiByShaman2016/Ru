0cc5fc4a9ed08e74a9612cca5155d7e7f37c6d0c
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-10-16 23:18:29 +0300

    Update metadata.js

--- JavaScript/2.0/metadata.js
+++ JavaScript/2.0/metadata.js
@@ -8,5 +8,9 @@ const url = new URL(window.location)
 const searchParams = url.searchParams
 if searchParams.get('id') == '1' {
   TextContent('ogtitle', '2.0 страницы')
-  TextContent('ogsitetitle', 'RuScratchWikiByShaman2016')
+  TextContent('ogname', 'RuScratchWikiByShaman2016')
+  TextContent('ogurl', 'https://scratchwikibyshaman2016.github.io/Ru/wiki/2.0?id=\"1\"')\
+  TextContent('ogdesc', 'Вики страницы 2.0')
+  TextContent('icon', '/icon.png')
+  TextContent('description', 'ScratchWikiByShaman2016, RuWiki, страницы 2.0')
 }
