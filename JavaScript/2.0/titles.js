ecfb4e7551b9c275d323d445a1fad3e2d46b9f47
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-10-16 23:07:48 +0300

    Update titles.js

--- JavaScript/2.0/titles.js
+++ JavaScript/2.0/titles.js
@@ -1,1 +1,11 @@
-
+function innerHTML(p1, p2) {
+  document.getElementById(p1).innerHTML = p2
+}
+function TextContent(p1, p2) {
+  document.getElementById(p1).textContent = p2
+}
+const url = new URL(window.location)
+const searchParams = url.searchParams
+if searchParams.get('id') == '1' {
+  TextContent('title', '2.0 вики-страницы')
+}
